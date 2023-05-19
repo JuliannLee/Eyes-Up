4f230523117ccf0896d8d4975872a7c9229189e2
JulianPhilbertLee <211110066@students.mikroskil.ac.id>
2023-05-19 23:43:21 +0700

    nothing

--- android/app/src/main/kotlin/com/example/p01/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.p01
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/utils/global.colors.dart
+++
@@ -1,9 +0,0 @@
-import 'package:hexcolor/hexcolor.dart';
-
-class GlobalColors {
-
-  static HexColor mainColor = HexColor('#3A98B9');
-  static HexColor textColor = HexColor('#210062');
-  static HexColor buttColor = HexColor('#9E4784');
-
-}
