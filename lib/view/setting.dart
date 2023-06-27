1a324e2984260ad6651076b826fef44f5af2f91f
Albert Suhargo <211110370@students.mikroskil.ac.id>
2023-06-27 22:29:54 +0700

    added body and basic layouts for settings page

--- lib/view/setting.dart
+++ lib/view/setting.dart
@@ -1,14 +1,130 @@
-// ignore: unnecessary_import
 import 'package:flutter/material.dart';
 
+class Setting extends StatefulWidget {
+  const Setting({super.key});
 
-// ignore: use_key_in_widget_constructors
-class Setting extends StatelessWidget {
+  @override
+  State<Setting> createState() => _SettingState();
+}
+
+class _SettingState extends State<Setting> {
+  String dropdownValue = "English";
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: 
-        Text('halaman setting')
-      );
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  height: 195,
+                  child: Column(
+                    children: [
+                      ListTile(
+                        title: Text(
+                          "Profile",
+                          style: TextStyle(color: Colors.blue, fontSize: 22),
+                        ),
+                      ),
+                      ListTile(
+                        title: Text(
+                          "Personal Details",
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      ),
+                      ListTile(
+                        title: Text(
+                          "Notifications",
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      ),
+                      ListTile(
+                        title: Text(
+                          "Change Themes",
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  height: 100,
+                  child: Column(
+                    children: [
+                      ListTile(
+                        title: Text(
+                          "Languages",
+                          style: TextStyle(color: Colors.blue, fontSize: 22),
+                        ),
+                      ),
+                      ListTile(
+                          title: Text(
+                            "Primary Language",
+                            style: TextStyle(fontSize: 16),
+                          ),
+                          trailing: DropdownButton<String>(
+                              value: dropdownValue,
+                              items: <String>[
+                                'English',
+                                'Bahasa Indonesia'
+                              ].map<DropdownMenuItem<String>>((String value) {
+                                return DropdownMenuItem<String>(
+                                  value: value,
+                                  child: Text(
+                                    value,
+                                    style: TextStyle(fontSize: 16),
+                                  ),
+                                );
+                              }).toList(),
+                              onChanged: (String? newValue) {
+                                setState(() {
+                                  dropdownValue = newValue!;
+                                });
+                              })),
+                    ],
+                  ),
+                ),
+                Container(
+                  height: 300,
+                  child: Column(children: [
+                    ListTile(
+                      title: Text(
+                        "Support and Feedback",
+                        style: TextStyle(color: Colors.blue, fontSize: 22),
+                      ),
+                    ),
+                    ListTile(
+                      title: Text("Terms & Privacy Policy"),
+                      leading: Icon(
+                        Icons.question_mark,
+                        size: 25,
+                      ),
+                    ),
+                    ListTile(
+                      title: Text("FAQ"),
+                      leading: Icon(
+                        Icons.chat_sharp,
+                        size: 25,
+                      ),
+                    ),
+                    ListTile(
+                      title: Text("Send Us Feedback"),
+                      leading: Icon(
+                        Icons.feedback,
+                        size: 25,
+                      ),
+                    ),
+                  ]),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
