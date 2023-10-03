8c1556461ccc98ab4024cb6389e72bf57c5ea6bf
StanlyWinata <211110222@students.mikroskil.ac.id>
2023-10-03 19:15:01 +0700

    menambah carausol dan perbaiki bug

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       home: SplashView(),
     );
   }
--- lib/view/community.volun.dart
+++ lib/view/community.volun.dart
@@ -1,8 +1,10 @@
 import 'dart:io';
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/routes/default_transitions.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class CommunityVolu extends StatefulWidget {
   const CommunityVolu({Key? key}) : super(key: key);
@@ -13,42 +15,64 @@ class CommunityVolu extends StatefulWidget {
 
 class _CommunityVoluState extends State<CommunityVolu> {
   List<Map<String, dynamic>> posts = [];
+  Map<int, int> currentIndexMap = {};
 
-  @override
-  void initState() {
-    super.initState();
-    loadPostData();
+  void toggleLove(int postIndex) {
+    setState(() {
+      if (posts[postIndex]['isLoved'] == null || !posts[postIndex]['isLoved']) {
+        // If not loved or null, mark as loved and increase the count
+        posts[postIndex]['isLoved'] = true;
+        posts[postIndex]['loveCount'] = (posts[postIndex]['loveCount'] ?? 0) + 1;
+      } else {
+        // If loved, unmark and decrease the count
+        posts[postIndex]['isLoved'] = false;
+        posts[postIndex]['loveCount'] = (posts[postIndex]['loveCount'] ?? 0) - 1;
+      }
+    });
+    savePostData(); // Save the updated posts list
   }
 
   @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    // Reload post data when the screen is resumed (e.g., when returning from Posting)
-    loadPostData();
+void initState() {
+  super.initState();
+  loadPostData();
+}
+
+@override
+void didChangeDependencies() {
+  super.didChangeDependencies();
+  // Reload post data when the screen is resumed (e.g., when returning from Posting)
+  loadPostData();
+}
+
+Future<void> loadPostData() async {
+  final prefs = await SharedPreferences.getInstance();
+  final jsonData = prefs.getString('posts');
+
+  if (jsonData != null) {
+    final List<dynamic> dataList = json.decode(jsonData);
+
+    final List<Map<String, dynamic>> loadedPosts = dataList
+        .map((data) {
+          if (data['images'] is List<dynamic>) {
+            data['images'] = data['images'].cast<String>();
+          }
+          return Map<String, dynamic>.from(data);
+        })
+        .cast<Map<String, dynamic>>()
+        .toList();
+
+    setState(() {
+      posts = loadedPosts;
+      
+      // Initialize currentIndex values for each CarouselSlider
+      for (int i = 0; i < posts.length; i++) {
+        currentIndexMap[i] = 0;
+      }
+    });
   }
+}
 
-  Future<void> loadPostData() async {
-    final prefs = await SharedPreferences.getInstance();
-    final jsonData = prefs.getString('posts');
-
-    if (jsonData != null) {
-      final List<dynamic> dataList = json.decode(jsonData);
-
-      final List<Map<String, dynamic>> loadedPosts = dataList
-          .map((data) {
-            if (data['image'] is String) {
-              data['image'] = File(data['image']);
-            }
-            return Map<String, dynamic>.from(data);
-          })
-          .cast<Map<String, dynamic>>()
-          .toList();
-
-      setState(() {
-        posts = loadedPosts;
-      });
-    }
-  }
 
   Future<void> savePostData() async {
     final prefs = await SharedPreferences.getInstance();
@@ -62,6 +86,26 @@ class _CommunityVoluState extends State<CommunityVolu> {
     await prefs.setString('posts', jsonData);
   }
 
+  List<Widget> _buildImageBubbles(int currentIndex, int totalImages) {
+  List<Widget> bubbles = [];
+
+  for (int i = 0; i < totalImages; i++) {
+    bubbles.add(
+      Container(
+        width: 10,
+        height: 10,
+        margin: EdgeInsets.symmetric(horizontal: 4),
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: i == currentIndex ? Colors.blue : Colors.grey,
+        ),
+      ),
+    );
+  }
+
+  return bubbles;
+}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -70,54 +114,98 @@ class _CommunityVoluState extends State<CommunityVolu> {
         backgroundColor: Colors.blue,
       ),
       body: posts.isEmpty
-          ? Center(
-              child: Text(
-                'No posts to display',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            )
-          : ListView.builder(
-              itemCount: posts.length,
-              itemBuilder: (context, index) {
-                final post = posts[index];
-                final image = post['image'];
-                final title = post['title'];
-                final description = post['description'];
-
-                if (image != null && image is File) {
-                  return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Card(
-                      child: Column(
+      ? Center(
+          child: Text(
+            'No posts to display',
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        )
+      : ListView.builder(
+          itemCount: posts.length,
+          itemBuilder: (context, index) {
+            final post = posts[index];
+            final images = post['images'];
+            final title = post['title'];
+            final description = post['description'];
+            final bool isLoved = post['isLoved'] ?? false;
+            final int loveCount = post['loveCount'] ?? 0;
+            if (images != null && images is List<String> && images.isNotEmpty) {
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Card(
+                  child: Column(
+                    children: [
+                      CarouselSlider(
+                        options: CarouselOptions(
+                          height: 200.0,
+                          viewportFraction: 1.0,
+                          enableInfiniteScroll: false,
+                          initialPage: currentIndexMap[index] ?? 0, // Use a default value of 0 if currentIndexMap[index] is null
+                          onPageChanged: (int imageIndex, CarouselPageChangedReason reason) {
+                            setState(() {
+                              // Update the current index for the specific set of images
+                              currentIndexMap[index] = imageIndex;
+                            });
+                          },
+                        ),
+                        items: images.map((imagePath) {
+                          return Image.file(File(imagePath));
+                        }).toList(),
+                      ),
+                      SizedBox(height: 8.0),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: _buildImageBubbles(currentIndexMap[index] ?? 0, images.length), // Use a default value of 0 if currentIndexMap[index] is null
+                      ),
+                      Row(
                         children: [
-                          Image.file(image),
-                          if (title != null && title.isNotEmpty)
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Text(
-                                title,
-                                style: TextStyle(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
+                          InkWell(
+                            onTap: () {
+                              // Handle love icon tap here
+                              toggleLove(index); // Call the toggleLove method
+                            },
+                            child: Icon(
+                              isLoved ? Icons.favorite : Icons.favorite_border, // Toggle icon based on isLoved state
+                              color: isLoved ? Colors.red : Colors.grey, // Toggle color based on isLoved state
                             ),
-                          if (description != null && description.isNotEmpty)
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Text(description),
-                            ),
+                          ),
+                          SizedBox(width: 8.0),
+                          Text(loveCount.toString()), // Display the love count
+                          SizedBox(width: 8.0),
+                          Icon(
+                            Icons.send,
+                            color: Colors.blue,
+                          ),
                         ],
                       ),
-                    ),
-                  );
-                }
-                return Container();
-              },
-            ),
+                      if (title != null && title.isNotEmpty)
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(20, 8, 0, 0),
+                          child: Align(alignment: Alignment.centerLeft,child:Text(
+                            title,
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),)
+                        ),
+                      if (description != null && description.isNotEmpty)
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(20, 8, 0, 0),
+                          child: Align(alignment: Alignment.centerLeft,child:Text(description),)
+                        ),
+                    ],
+                  ),
+                ),
+              );
+            }
+            return Container();
+          },
+        ),
+
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
           final Map<String, dynamic>? returnedPostData = await Navigator.push(
@@ -132,6 +220,7 @@ class _CommunityVoluState extends State<CommunityVolu> {
               posts.add(returnedPostData);
             });
             savePostData(); // Save the updated posts list
+            loadPostData(); // Refresh the data
           }
         },
         child: Icon(Icons.add),
@@ -152,15 +241,15 @@ class Posting extends StatefulWidget {
 class _PostingState extends State<Posting> {
   TextEditingController titleController = TextEditingController();
   TextEditingController descriptionController = TextEditingController();
-  File? selectedImage; // Store the image path as a String
+  List<File> selectedImages = []; // Store a list of selected images as File
 
-  Future<void> _selectImage() async {
+  Future<void> _selectImages() async {
     final imagePicker = ImagePicker();
-    final pickedImage = await imagePicker.getImage(source: ImageSource.gallery);
+    final pickedImages = await imagePicker.pickMultiImage(); // No need to specify source
 
-    if (pickedImage != null) {
+    if (pickedImages != null) {
       setState(() {
-        selectedImage = File(pickedImage.path); // Store the image as a File
+        selectedImages.addAll(pickedImages.map((image) => File(image.path)));
       });
     }
   }
@@ -168,6 +257,7 @@ class _PostingState extends State<Posting> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         title: Text('Post'),
         backgroundColor: Colors.blue,
@@ -175,21 +265,34 @@ class _PostingState extends State<Posting> {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
-          children: <Widget>[
-            GestureDetector(
-              onTap: _selectImage,
-              child: Container(
-                width: 200.0,
-                height: 200.0,
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.black),
-                ),
-                child: selectedImage != null
-                    ? Image.file(selectedImage!, fit: BoxFit.cover)
-                    : Icon(Icons.add_a_photo, size: 50.0),
+  children: <Widget>[
+    Row(
+      children: selectedImages
+          .map(
+            (image) => Container(
+              width: 100.0,
+              height: 100.0,
+              margin: EdgeInsets.all(8.0),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black),
               ),
+              child: Image.file(image, fit: BoxFit.cover),
             ),
-            SizedBox(height: 16.0),
+          )
+          .toList(),
+          ),
+          InkWell(
+            onTap: _selectImages,
+            child: Container(
+              width: 100.0,
+              height: 100.0,
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black),
+              ),
+              child: Icon(Icons.add_a_photo, size: 50.0),
+            ),
+          ),
+          SizedBox(height: 16.0),
             TextFormField(
               controller: titleController,
               decoration: InputDecoration(labelText: 'Title'),
@@ -207,19 +310,19 @@ class _PostingState extends State<Posting> {
                 final description = descriptionController.text;
 
                 final postMap = {
-                  'image': selectedImage, // Use the stored image as a File
-                  'title': title,
-                  'description': description,
+                'images': selectedImages.map((image) => image.path).toList(),
+                'title': title,
+                'description': description,
                 };
-
-                Navigator.pop(context, postMap); // Pass postMap back to the previous screen
+                Navigator.pop(context, postMap);
+                 // Pass postMap back to the previous screen
               },
               child: Text('Create Post'),
             ),
+
           ],
         ),
       ),
     );
   }
 }
-
